f9a5c0c1118c9134f3d9a081b12bbd8f275dd95e
mulajfx <mulajkillian@gmail.com>
2026-06-11 21:37:25 +0200

    feat: docent- en studentroutes toegevoegd

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      redirect: '/login', // ← add this
+      redirect: '/login',
     },
     {
       path: '/login',
@@ -24,10 +23,76 @@ const router = createRouter({
       component: () => import('../views/StudentAanvraag.vue'),
     },
     {
-  path: '/aanvragen',
-  name: 'commissie-aanvragen',
-  component: () => import('@/views/CommissieAanvragenView.vue')
-}
+      path: '/aanvragen',
+      name: 'commissie-aanvragen',
+      component: () => import('@/views/CommissieAanvragenView.vue'),
+    },
+    {
+      path: '/docent/studenten',
+      name: 'docent-studenten',
+      component: () => import('../views/docent/StudentenOverzichtView.vue'),
+    },
+    {
+      path: '/docent/studenten/:id',
+      name: 'docent-student-detail',
+      component: () => import('../views/docent/StudentDetailView.vue'),
+    },
+    {
+      path: '/docent',
+      name: 'docent-dashboard',
+      component: () => import('../views/PlaceholderView.vue'),
+    },
+    {
+      path: '/docent/logboek',
+      name: 'docent-logboek',
+      component: () => import('../views/PlaceholderView.vue'),
+    },
+    {
+      path: '/docent/evaluatie',
+      name: 'docent-evaluatie',
+      component: () => import('../views/PlaceholderView.vue'),
+    },
+    {
+      path: '/docent/documenten',
+      name: 'docent-documenten',
+      component: () => import('../views/PlaceholderView.vue'),
+    },
+    {
+      path: '/student/logboek',
+      name: 'student-logboek',
+      component: () => import('../views/PlaceholderView.vue'),
+    },
+    {
+      path: '/student/evaluatie',
+      name: 'student-evaluatie',
+      component: () => import('../views/PlaceholderView.vue'),
+    },
+    {
+      path: '/student/evaluatie-eind',
+      name: 'student-evaluatie-eind',
+      component: () => import('../views/PlaceholderView.vue'),
+    },
+    {
+      path: '/mentor',
+      name: 'mentor-dashboard',
+      component: () => import('../views/PlaceholderView.vue'),
+    },
+    {
+      path: '/admin',
+      name: 'admin-dashboard',
+      component: () => import('../views/PlaceholderView.vue'),
+    },
+    {
+      path: '/commissie',
+      name: 'commissie-dashboard',
+      component: () => import('../views/PlaceholderView.vue'),
+    },
+    {
+      path: '/wachtwoord-vergeten',
+      name: 'wachtwoord-vergeten',
+      component: () => import('../views/PlaceholderView.vue'),
+    },
+
   ],
 })
 
