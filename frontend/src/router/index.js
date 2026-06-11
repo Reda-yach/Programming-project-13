659e1d3c74b3911669b638ae826831bb28971727
mulajfx <mulajkillian@gmail.com>
2026-06-11 13:45:49 +0200

    fix : path toegevoegd voor login

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      redirect: '/login', // ← add this
+    },
     {
       path: '/login',
       name: 'login',
