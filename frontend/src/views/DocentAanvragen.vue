7aa2b1b7904c7a8a3ba5d242a7722c97481ad48c
Reda-yach <yachaouireda@gmail.com>
2026-06-22 08:23:30 +0200

    cleanup: onnodige view bestanden verwijderd
    - DocentAanvragen.vue: was enkel een redirect-wrapper naar /docent-aanvragen,
      router wijst nu rechtstreeks naar DocentInCommissieAanvragen.vue
    - MentorLogboek.vue: oude versie, niet in de router (router gebruikt MentorLogboeken.vue)
    
    Co-Authored-By: Reda-yach <yachaouireda@gmail.com>

--- frontend/src/views/DocentAanvragen.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import { onMounted } from 'vue'
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-onMounted(() => router.replace('/docent-aanvragen'))
-</script>
-
-<template><div></div></template>
